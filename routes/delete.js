a293ed57bc24a2fad809b7cb361168b79a14e096
czer <czerpdx@gmail.com>
2023-06-01 15:02:27 -0700

    added console logs for live dev debugging

--- routes/delete.js
+++ routes/delete.js
@@ -10,6 +10,7 @@ router.delete('/:bucketName/:filename', async (req, res) => {
 
     // If the bucket doesn't exist or wasn't included in the request parameters
     if (!fs.existsSync(bucketName) || !req.params.bucketName) {
+      console.error('bucket name was invalid or not provided');
       return res.status(404).send({
         Error: {
           Code: 'NoSuchBucket',
@@ -21,6 +22,7 @@ router.delete('/:bucketName/:filename', async (req, res) => {
 
     // If the filename was not included in the request parameters
     if (!req.params.filename) {
+      console.error('filename was not provided');
       return res.status(400).send({
         Error: {
           Code: 'MethodNotAllowed',
@@ -32,18 +34,19 @@ router.delete('/:bucketName/:filename', async (req, res) => {
 
     // If the file doesn't exist return success (it's already gone!)
     if (!fs.existsSync(filepath)) {
-      console.log('file does not exist on the server');
+      console.error('file does not exist on the server');
       return res.status(204).end();
     }
 
     // Delete the file from the server
+    console.log(`deleting file: ${filepath}`);
     fs.unlinkSync(filepath);
+    console.log(`file deleted.`);
     return res.status(204).end();
   } catch (error) {
-    
+
     // Log the error for debugging purposes
     console.error(error);
-
     return res.status(500).send({
       Error: {
         Code: 'InternalFailure',
